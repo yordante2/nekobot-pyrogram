a266f7478a06b03fc8d771e54309ab72ba9aeb64
aliciarolineko <alicia.neko@mail.ru>
2024-10-08 07:35:15 -0400

    Update neko.py 🤔

--- neko.py
+++ neko.py
@@ -236,10 +236,17 @@ async def covernh_operation(client, message, codes):
             with open(img_filename, 'wb') as img_file:
                 img_file.write(img_data)
 
-            await client.send_photo(message.chat.id, img_filename, caption=f"https://nhentai.net/g/{code} {page_name}")
+            await client.send_photo(message.chat.id, img_filename, caption=f"https://nhentai.net/g/{code} {page_name}") except Exception as e: await client.send_document(message.chat.id, img_filename, caption=f"https://nhentai.net/g/{code} {page_name}")
         else:
             await message.reply(f"No se encontró ninguna imagen para el código {code}")
 
+
+try:
+    await client.send_photo(message.chat.id, img_filename, caption=f"https://n.net/g/{code} {page_name}")
+except Exception as e:
+    print(f"Error al enviar la imagen: {e}")
+    await client.send_document(message.chat.id, img_filename, caption=f"https://n.net/g/{code} {page_name}")
+    
     
 def sanitize_input(input_string):
     return re.sub(r'[^a-zA-Z0-9\[\] ]', '', input_string)
