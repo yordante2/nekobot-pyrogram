9ecf7444b67222118a379e5a347983ddeed95e9e
Naki <alicia.neko@mail.ru>
2024-10-27 15:26:54 -0400

    Update neko.py 😒

--- neko.py
+++ neko.py
@@ -199,7 +199,7 @@ async def handle_compress(client, message, username):
 async def handle_up(client, message):
     if message.reply_to_message:
         await message.reply("Descargando...")
-        file_path = await client.download_media(message.reply_to_message.media.file_id)
+        file_path = await client.download_media(message.reply_to_message.document.file_id)
         await message.reply("Subiendo a la nube...")
         link = upload_token(file_path, os.getenv("NUBETOKEN"), os.getenv("NUBELINK"))
         await message.reply("Enlace:\n" + str(link).replace("/webservice", ""))
