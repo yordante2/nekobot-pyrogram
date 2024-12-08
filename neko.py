cb0719215369aeb42a95440f8c2cce73c10c7334
Naki <alicia.neko@mail.ru>
2024-12-08 15:58:13 -0500

    Update neko.py

--- neko.py
+++ neko.py
@@ -729,20 +729,20 @@ CODEWORD = os.getenv("CODEWORD")
 
 @app.on_message(filters.command("access") & filters.private)
 def access_command(client, message):
+    user_id = message.from_user.id
+    
     # Verificar si el mensaje contiene la palabra secreta
     if len(message.command) > 1 and message.command[1] == CODEWORD:
-        # Obtener el ID del usuario remitente
-        user_id = message.from_user.id
-        
         # Añadir el ID del usuario a la lista temp_users si no está ya añadido
         if user_id not in temp_users:
             temp_users.append(user_id)
+            allowed_users.append(user_id)  # Añadir también a allowed_users
             message.reply("Acceso concedido.")
         else:
             message.reply("Ya estás en la lista de acceso temporal.")
     else:
         message.reply("Palabra secreta incorrecta.")
-
+            
 
 @app.on_message(filters.text)
 async def handle_message(client, message):
