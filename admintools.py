2f6fb88b5b3d5ede5811d557e1cc525a3580cc29
Naki <alicia.neko@mail.ru>
2025-03-24 10:29:10 -0400

    Update admintools.py

--- admintools.py
+++ admintools.py
@@ -1,35 +1,17 @@
 import os
-import glob
-from pyrogram import Client, filters
-import zipfile
-import shutil
-import random
-import string
-import smtplib
-from email.message import EmailMessage
-import requests
-from bs4 import BeautifulSoup
-import re
-from moodleclient import upload_token
-import datetime
-import subprocess
+from pyrogram import Client
 from pyrogram.types import Message
 import asyncio
-import os
-import hashlib
-import py7zr
-import shutil
-import string
-import random
-import aiohttp
-import aiofiles
 
+async def handle_start(client, message):
+    await message.reply("Funcionando")
+    
 async def add_user(client, message, user_id):
     new_user_id = int(message.text.split()[1])
     temp_users.append(new_user_id)
     allowed_users.append(new_user_id)
     await message.reply(f"Usuario {new_user_id} añadido temporalmente.")
-    
+
 async def remove_user(client, message, user_id):
     rem_user_id = int(message.text.split()[1])
     if rem_user_id in temp_users:
@@ -38,13 +20,13 @@ async def remove_user(client, message, user_id):
         await message.reply(f"Usuario {rem_user_id} eliminado temporalmente.")
     else:
         await message.reply("Usuario no encontrado en la lista temporal.")
-        
+
 async def add_chat(client, message, user_id):
     chat_id = message.chat.id
     temp_chats.append(chat_id)
     allowed_users.append(chat_id)
     await message.reply(f"Chat {chat_id} añadido temporalmente.")
-    
+
 async def remove_chat(client, message, user_id):
     chat_id = message.chat.id
     if chat_id in temp_chats:
@@ -53,13 +35,13 @@ async def remove_chat(client, message, user_id):
         await message.reply(f"Chat {chat_id} eliminado temporalmente.")
     else:
         await message.reply("Chat no encontrado en la lista temporal.")
-        
+
 async def ban_user(client, message, user_id):
     ban_user_id = int(message.text.split()[1])
     if ban_user_id not in admin_users:
         ban_users.append(ban_user_id)
         await message.reply(f"Usuario {ban_user_id} baneado.")
-        
+
 async def deban_user(client, message, user_id):
     deban_user_id = int(message.text.split()[1])
     if deban_user_id in ban_users:
@@ -67,3 +49,4 @@ async def deban_user(client, message, user_id):
         await message.reply(f"Usuario {deban_user_id} desbaneado.")
     else:
         await message.reply("Usuario no encontrado en la lista de baneados.")
+        
