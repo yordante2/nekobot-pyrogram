7cdcc5f743bfa861efa52a3ef5b42b611da62e96
Naki <alicia.neko@mail.ru>
2025-03-29 10:40:51 -0400

    Update video_processor.py

--- command/video_processor.py
+++ command/video_processor.py
@@ -1,49 +1,19 @@
-import asyncio
-import os
-import subprocess
-import re
-import datetime
-   
-def human_readable_size(size, decimal_places=2):
-    """
-    Convierte bytes en un formato legible (KB, MB, GB, etc.).
-    """
-    for unit in ['KB', 'MB', 'GB', 'TB']:
-        if size < 1024.0:
-            return f"{size:.{decimal_places}f} {unit}"
-        size /= 1024.0
-        
+from command.get_files.video_file import obtener_duracion_video, comprimir_video, calcular_progreso
 
 async def procesar_video(client, message, original_video_path, task_id, tareas_en_ejecucion):
     chat_id = message.chat.id
     compressed_video_path = f"{os.path.splitext(original_video_path)[0]}_compressed.mkv"
-    ffmpeg_command = [
-        'ffmpeg', '-y', '-i', original_video_path,
-        '-s', "640x400",  # Ajusta según tus settings dinámicos si es necesario
-        '-crf', "28",
-        '-b:a', "80k",
-        '-r', "18",
-        '-preset', "veryfast",
-        '-c:v', "libx265",
-        compressed_video_path
-    ]
 
     progress_message = await client.send_message(chat_id=chat_id, text="🚀 **Iniciando proceso de compresión...**")
 
     try:
-        # Obtener duración total del video usando ffprobe
-        total_duration = subprocess.check_output(
-            ["ffprobe", "-v", "error", "-show_entries", "format=duration",
-             "-of", "default=noprint_wrappers=1:nokey=1", original_video_path]
-        )
-        total_duration = float(total_duration.strip())
-
+        total_duration = obtener_duracion_video(original_video_path)
         start_time = datetime.datetime.now()
-        process = subprocess.Popen(ffmpeg_command, stderr=subprocess.PIPE, text=True)
+        process = comprimir_video(original_video_path, compressed_video_path)
+
         last_update_time = datetime.datetime.now()
 
         while True:
-            # Cancelar tarea si se marca como cancelada
             if tareas_en_ejecucion[task_id]["cancel"]:
                 process.terminate()
                 await progress_message.edit_text(f"❌ Proceso cancelado para `{task_id}`.")
@@ -56,72 +26,39 @@ async def procesar_video(client, message, original_video_path, task_id, tareas_e
             if output == '' and process.poll() is not None:
                 break
 
-            if "size=" in output and "time=" in output:
-                match = re.search(r"size=\s*([\d]+).*time=([\d:.]+)", output)
-                if match:
-                    size_kb, current_time_str = match.groups()
-                    size_kb = int(size_kb)
-                    readable_size = human_readable_size(size_kb)
+            readable_size, percentage, current_time = calcular_progreso(output, total_duration)
+            if (datetime.datetime.now() - last_update_time).seconds >= 10:
+                elapsed_time = datetime.datetime.now() - start_time
+                estimated_total_time = (elapsed_time.total_seconds() / (percentage / 100)) if percentage > 0 else 0
+                remaining_time = str(datetime.timedelta(seconds=int(estimated_total_time - elapsed_time.total_seconds())))
 
-                    current_time_parts = list(map(float, current_time_str.split(':')))
-                    current_time = (
-                        current_time_parts[0] * 3600 +
-                        current_time_parts[1] * 60 +
-                        current_time_parts[2]
+                await progress_message.edit_text(
+                    text=(
+                        f"🚀 **Progreso:**\n"
+                        f"📊 Tamaño procesado: `{readable_size}`\n"
+                        f"📈 Porcentaje completado: `{percentage:.2f}%`\n"
+                        f"⏳ Tiempo total transcurrido: `{str(elapsed_time).split('.')[0]}`\n"
+                        f"⌛ Tiempo estimado restante: `{remaining_time}`\n"
                     )
-
-                    percentage = (current_time / total_duration) * 100
-                    elapsed_time = datetime.datetime.now() - start_time
-                    elapsed_seconds = elapsed_time.total_seconds()
-                    estimated_total_time = elapsed_seconds / (percentage / 100) if percentage > 0 else 0
-                    remaining_seconds = estimated_total_time - elapsed_seconds
-                    remaining_time = str(datetime.timedelta(seconds=int(remaining_seconds)))
-
-                    if (datetime.datetime.now() - last_update_time).seconds >= 10:
-                        try:
-                            await progress_message.edit_text(
-                                text=(
-                                    f"🚀 **Progreso:**\n"
-                                    f"📊 Tamaño procesado: `{readable_size}`\n"
-                                    f"⏱️ Tiempo actual: `{current_time_str}` / `{str(datetime.timedelta(seconds=total_duration))}`\n"
-                                    f"📈 Porcentaje completado: `{percentage:.2f}%`\n"
-                                    f"⏳ Tiempo total transcurrido: `{str(elapsed_time).split('.')[0]}`\n"
-                                    f"⌛ **Tiempo estimado restante:** `{remaining_time}`\n\n"
-                                    f"🔄 El mensaje de progreso se actualiza cada 10 segundos...\n"
-                                    f"❌ `/cancel {task_id}`"
-                                )
-                            )
-                            last_update_time = datetime.datetime.now()
-                        except Exception as e:
-                            if "MESSAGE_NOT_MODIFIED" in str(e):
-                                pass
-                            else:
-                                raise
+                )
+                last_update_time = datetime.datetime.now()
 
         await progress_message.edit_text("✅ **Proceso completado. Preparando resultados...**")
-        await asyncio.sleep(5)
-        await progress_message.delete()
 
         compressed_size = os.path.getsize(compressed_video_path)
-        duration_str = str(datetime.timedelta(seconds=total_duration))
-        processing_time = datetime.datetime.now() - start_time
-        processing_time_str = str(processing_time).split('.')[0]
-
-        original_size_display = human_readable_size(os.path.getsize(original_video_path) // 1024)
-        compressed_size_display = human_readable_size(os.path.getsize(compressed_video_path) // 1024)
-
+        original_size = os.path.getsize(original_video_path)
         description = (
             f"✅ **Archivo procesado correctamente ☑️**\n"
-            f"📂 **Tamaño original:** {original_size_display}\n"
-            f"📁 **Tamaño procesado:** {compressed_size_display}\n"
-            f"⌛ **Tiempo de procesamiento:** {processing_time_str}\n"
-            f"🎥 **Duración del video:** {duration_str}\n"
+            f"📂 **Tamaño original:** {human_readable_size(original_size // 1024)}\n"
+            f"📁 **Tamaño procesado:** {human_readable_size(compressed_size // 1024)}\n"
+            f"⌛ **Tiempo de procesamiento:** {str(datetime.datetime.now() - start_time).split('.')[0]}\n"
+            f"🎥 **Duración del video:** {str(datetime.timedelta(seconds=total_duration))}\n"
             f"🎉 **¡Gracias por usar el bot!**"
         )
+
         nombre = os.path.splitext(os.path.basename(compressed_video_path))[0]
         return nombre, description, chat_id, compressed_video_path, original_video_path
     except Exception as e:
         await client.send_message(chat_id=chat_id, text=f"❌ **Ocurrió un error al procesar el video:**\n{e}")
         os.remove(original_video_path)
         os.remove(compressed_video_path)
-    
