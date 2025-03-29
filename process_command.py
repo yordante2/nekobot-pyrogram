27dac74bfd3250a6963a18d8c0821599b68f68eb
Naki <alicia.neko@mail.ru>
2025-03-29 08:08:17 -0400

    Update process_command.py

--- process_command.py
+++ process_command.py
@@ -88,7 +88,6 @@ async def process_command(client: Client, message: Message, active_cmd: str, adm
             elif text.startswith("/rename"):
                 await asyncio.create_task(rename(client, message))
         return
-        )
 
     if text.startswith(("/convert", "/calidad", "/autoconvert", "/cancel", "/list")) or (message.video is not None):
         if cmd("videotools", user_id in admin_users, user_id in vip_users):
