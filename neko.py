5bba7606f9e7fd3758dfcb5a3acd710810cc0acb
Naki <alicia.neko@mail.ru>
2025-03-30 19:41:14 -0400

    Update neko.py

--- neko.py
+++ neko.py
@@ -1,35 +1,37 @@
 import os
-from pyrogram import Client, filters
-from process_command import process_command
 import asyncio
 import nest_asyncio
+from pyrogram import Client, filters
+from process_command import process_command
+from command.htools import manejar_opcion
+from command.help import handle_help_callback
 
 nest_asyncio.apply()
 
+# Configuración de variables de entorno
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 bot_token = os.getenv('TOKEN')
 admin_users = list(map(int, os.getenv('ADMINS').split(','))) if os.getenv('ADMINS') else []
 users = list(map(int, os.getenv('USERS').split(','))) if os.getenv('USERS') else []
-temp_users = []
-temp_chats = []
-ban_users = []
-allowed_users = admin_users + users + temp_users + temp_chats
-app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
-
-CODEWORD = os.getenv('CODEWORD', '')
-BOT_IS_PUBLIC = os.getenv('BOT_IS_PUBLIC', 'false')
-
 vip_users = list(map(int, os.getenv('VIP_USERS', '').split(','))) if os.getenv('VIP_USERS') else []
-allowed_ids = set(admin_users).union(set(vip_users))
-protect_content_env = os.getenv('PROTECT_CONTENT', '').strip().lower()
-is_protect_content_enabled = protect_content_env == 'true'
+temp_users, temp_chats, ban_users = [], [], []
 
 MAIN_ADMIN = os.getenv("MAIN_ADMIN")
+CODEWORD = os.getenv('CODEWORD', '')
+BOT_IS_PUBLIC = os.getenv('BOT_IS_PUBLIC', 'false').strip().lower() == "true"
+PROTECT_CONTENT = os.getenv('PROTECT_CONTENT', '').strip().lower() == "true"
 
+allowed_users = admin_users + users + temp_users + temp_chats
+allowed_ids = set(admin_users).union(set(vip_users))
+
+app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+
+# Función para verificar si el bot es público
 def is_bot_public():
-    return BOT_IS_PUBLIC and BOT_IS_PUBLIC.lower() == "true"
+    return BOT_IS_PUBLIC
 
+# Comando para procesar acceso temporal
 async def process_access_command(message):
     user_id = message.from_user.id
     if len(message.command) > 1 and message.command[1] == CODEWORD:
@@ -41,35 +43,29 @@ async def process_access_command(message):
             await message.reply("Ya estás en la lista de acceso temporal.")
     else:
         await message.reply("Palabra secreta incorrecta.")
-        
-async def notify_main_admin(app):
+
+# Notificar al administrador principal cuando el bot inicie
+async def notify_main_admin():
     if MAIN_ADMIN:
         try:
-            if MAIN_ADMIN.isdigit():
-                # MAIN_ADMIN is a user ID
-                await app.send_message(chat_id=int(MAIN_ADMIN), text=f"Bot @{app.me.username} iniciado")
-            else:
-                # MAIN_ADMIN is a username
-                await app.send_message(chat_id=MAIN_ADMIN, text=f"Bot @{app.me.username} iniciado")
+            chat_id = int(MAIN_ADMIN) if MAIN_ADMIN.isdigit() else MAIN_ADMIN
+            await app.send_message(chat_id=chat_id, text=f"Bot @{app.me.username} iniciado")
         except Exception as e:
             print(f"Error al enviar el mensaje al MAIN_ADMIN: {e}")
 
-from command.htools import manejar_opcion
-
+# Manejador de callbacks con protección de contenido
 @app.on_callback_query(filters.regex("^(cbz|pdf|fotos)"))
 async def callback_handler(client, callback_query):
     user_id = callback_query.from_user.id
-    if is_protect_content_enabled and user_id not in allowed_ids:
-        protect_content = True
-    else:
-        protect_content = False
+    protect_content = PROTECT_CONTENT and user_id not in allowed_ids
     await manejar_opcion(client, callback_query, protect_content, user_id)
 
+# Manejador de ayuda con callbacks
 @app.on_callback_query()
-async def callback_handler(client, callback_query):
-    from command.help import handle_help_callback
-    await asyncio.create_task(handle_help_callback(client, callback_query))
+async def help_callback_handler(client, callback_query):
+    await handle_help_callback(client, callback_query)
 
+# Manejador de mensajes
 @app.on_message()
 async def handle_message(client, message):
     user_id = message.from_user.id
@@ -89,11 +85,18 @@ async def handle_message(client, message):
 
     active_cmd = os.getenv('ACTIVE_CMD', '').lower()
     admin_cmd = os.getenv('ADMIN_CMD', '').lower()
-    await asyncio.create_task(process_command(client, message, active_cmd, admin_cmd, user_id, username, chat_id))
+    await process_command(client, message, active_cmd, admin_cmd, user_id, username, chat_id)
 
-try:
-    app.start()
+# Función principal para iniciar el bot
+async def main():
+    await app.start()
     if MAIN_ADMIN:
-        notify_main_admin(app)
-except KeyboardInterrupt:
-    print("Detención forzada realizada")
+        await notify_main_admin()
+    print("Bot iniciado y operativo.")
+    await app.idle()
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Detención forzada realizada")
