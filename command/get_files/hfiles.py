ad65f89ad315758c811175397360f1b417eab32f
Naki <alicia.neko@mail.ru>
2025-03-30 17:09:11 -0400

    Update hfiles.py

--- command/get_files/hfiles.py
+++ command/get_files/hfiles.py
@@ -56,7 +56,7 @@ def descargar_hentai(url, code, base_url, operation_type, protect_content, folde
                 img_file.write(img_data)
 
         if operation_type == "cover":
-            page_title = f"{page_title} https://{base_url}/{code}/"
+            page_title = f"{page_title} \n https://{base_url}/{code}/"
             page_title = re.sub("Page 1  nhentai hentai doujinshi and manga|Page 1  3Hentai", "", page_title)
             
             results = {"caption": page_title, "img_file": first_img_filename}
