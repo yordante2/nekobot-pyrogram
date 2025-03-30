6fa5df49cf0868d8bbe1dfa2a437d1311680ac0c
Naki <alicia.neko@mail.ru>
2025-03-30 19:24:36 -0400

    Update neko.py

--- neko.py
+++ neko.py
@@ -25,6 +25,8 @@ allowed_ids = set(admin_users).union(set(vip_users))
 protect_content_env = os.getenv('PROTECT_CONTENT', '').strip().lower()
 is_protect_content_enabled = protect_content_env == 'true'
 
+MAIN_ADMIN = int(os.getenv("MAIN_ADMIN")) if os.getenv("MAIN_ADMIN") else None
+
 def is_bot_public():
     return BOT_IS_PUBLIC and BOT_IS_PUBLIC.lower() == "true"
 
@@ -40,6 +42,12 @@ async def process_access_command(message):
     else:
         await message.reply("Palabra secreta incorrecta.")
 
+async def notify_main_admin(app):
+    if MAIN_ADMIN:
+        try:
+            await app.send_message(chat_id=MAIN_ADMIN, text="Bot iniciado")
+        except Exception as e:
+            print(f"Error al enviar el mensaje al MAIN_ADMIN: {e}")
 
 from command.htools import manejar_opcion
 
@@ -48,16 +56,14 @@ async def callback_handler(client, callback_query):
     user_id = callback_query.from_user.id
     if is_protect_content_enabled and user_id not in allowed_ids:
         protect_content = True
-
     else:
         protect_content = False
     await manejar_opcion(client, callback_query, protect_content, user_id)
-    
+
 @app.on_callback_query()
 async def callback_handler(client, callback_query):
     from command.help import handle_help_callback
     await asyncio.create_task(handle_help_callback(client, callback_query))
-
 
 @app.on_message()
 async def handle_message(client, message):
@@ -81,6 +87,8 @@ async def handle_message(client, message):
     await asyncio.create_task(process_command(client, message, active_cmd, admin_cmd, user_id, username, chat_id))
 
 try:
-    app.run()
+    app.start()
+    if MAIN_ADMIN:
+        app.loop.run_until_complete(notify_main_admin(app))
 except KeyboardInterrupt:
     print("Detención forzada realizada")
