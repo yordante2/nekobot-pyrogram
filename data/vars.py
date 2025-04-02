370011e28a64ad97c4c8d58958c4c811b89dadce
Naki <alicia.neko@mail.ru>
2025-04-02 07:13:58 -0400

    Create vars.py

---
+++ data/vars.py
@@ -0,0 +1,16 @@
+# Configuración de variables de entorno
+api_id = os.getenv('API_ID')
+api_hash = os.getenv('API_HASH')
+bot_token = os.getenv('TOKEN')
+admin_users = list(map(int, os.getenv('ADMINS').split(','))) if os.getenv('ADMINS') else []
+users = list(map(int, os.getenv('USERS').split(','))) if os.getenv('USERS') else []
+vip_users = list(map(int, os.getenv('VIP_USERS', '').split(','))) if os.getenv('VIP_USERS') else []
+temp_users, temp_chats, ban_users = [], [], []
+
+MAIN_ADMIN = os.getenv("MAIN_ADMIN")
+CODEWORD = os.getenv('CODEWORD', '')
+BOT_IS_PUBLIC = os.getenv('BOT_IS_PUBLIC', 'false').strip().lower() == "true"
+PROTECT_CONTENT = os.getenv('PROTECT_CONTENT', '').strip().lower() == "true"
+
+allowed_users = admin_users + users + temp_users + temp_chats
+allowed_ids = set(admin_users).union(set(vip_users))
