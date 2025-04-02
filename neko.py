4d8464fa1e40eeb958aa464c092164f50d4f7da6
Naki <alicia.neko@mail.ru>
2025-04-02 07:13:51 -0400

    Update neko.py

--- neko.py
+++ neko.py
@@ -8,25 +8,8 @@ from command.help import handle_help_callback, handle_help
 import time  # Importación necesaria
 from stickers import saludos
 import random
-
+from data.vars import api_id, api_hash, bot_token, admin_users, users, temp_users, vip_users, MAIN_ADMIN, CODEWORD, BOT_IS_PUBLIC, PROTECT_CONTENT, allowed_ids, allowed_users
 nest_asyncio.apply()
-
-# Configuración de variables de entorno
-api_id = os.getenv('API_ID')
-api_hash = os.getenv('API_HASH')
-bot_token = os.getenv('TOKEN')
-admin_users = list(map(int, os.getenv('ADMINS').split(','))) if os.getenv('ADMINS') else []
-users = list(map(int, os.getenv('USERS').split(','))) if os.getenv('USERS') else []
-vip_users = list(map(int, os.getenv('VIP_USERS', '').split(','))) if os.getenv('VIP_USERS') else []
-temp_users, temp_chats, ban_users = [], [], []
-
-MAIN_ADMIN = os.getenv("MAIN_ADMIN")
-CODEWORD = os.getenv('CODEWORD', '')
-BOT_IS_PUBLIC = os.getenv('BOT_IS_PUBLIC', 'false').strip().lower() == "true"
-PROTECT_CONTENT = os.getenv('PROTECT_CONTENT', '').strip().lower() == "true"
-
-allowed_users = admin_users + users + temp_users + temp_chats
-allowed_ids = set(admin_users).union(set(vip_users))
 
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
